ba032e597568f3697c17286a55597089c1674e5e
vfacu44 <vfacu44@gmail.com>
2025-02-24 17:32:33 +0000

    Feat: añadida funcion de cerrar sesion -se actualizo el itme en el html. -se añadió la lógica para cerrar sesión en el componente ts.

--- src/app/modulos/administrativo/components/sidebar/sidebar.component.ts
+++ src/app/modulos/administrativo/components/sidebar/sidebar.component.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterModule } from '@angular/router';
-
+import { AuthService } from '../../../../services/auth.service'; // Ajusta la ruta según tu estructura
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [
-    RouterModule,
-    RouterLink
-],
+  imports: [RouterModule, RouterLink],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
 
+  constructor(
+    private authService: AuthService, // Para cerrar sesión
+    private router: Router // Para redirigir
+  ) {}
+
+  logout() {
+    this.authService.logout(); // Borra el token
+    this.router.navigate(['/login']); // Redirige a login
+  }
 }
