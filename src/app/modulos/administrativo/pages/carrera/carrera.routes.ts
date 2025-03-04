ee65d6c8e63beb4b7c59c094b7185bf24c50aa19
vfacu44 <vfacu44@gmail.com>
2025-03-04 15:56:45 +0000

    feat: quitar boton editar del navbar de alumnos, agregar componentes y formularios para la sección de 'carreras' -quitado el botón de editar del navbar de alumnos - Agregados componentes en layout de carrera (HTML y TS) - Agregado formulario de agregar carrera (HTML y TS) - Configuradas rutas para la sección de 'carreras'

--- src/app/modulos/administrativo/pages/carrera/carrera.routes.ts
+++ src/app/modulos/administrativo/pages/carrera/carrera.routes.ts
@@ -1,7 +1,15 @@
 import { Routes } from '@angular/router';
-import  CarreraLayoutComponent  from './carrera-layout/carrera-layout.component';
-import { AgregarCarreraComponent } from './pagesCarrera/agregar-carrera/agregar-carrera.component';
+import CarreraLayoutComponent  from './carrera-layout/carrera-layout.component';
+import AgregarCarreraComponent from './pagesCarrera/agregar-carrera/agregar-carrera.component';
+import { ListarCarreraComponent } from './pagesCarrera/listar-carrera/listar-carrera.component';
+import { EditarCarreraComponent } from './pagesCarrera/editar-carrera/editar-carrera.component';
 export const CARRERA_ROUTES: Routes = [
-    {path:'',component:CarreraLayoutComponent},
-    {path:'agregar', component:AgregarCarreraComponent}
+    {//al cargar la ruta base de carrera carga el layout de carrera y en diferido sus rutas hijas
+        path: '', component: CarreraLayoutComponent,
+        children: [//si no se definen como rutas hijas van a tapar el contenido de carrera layout
+            { path: 'listar', component: ListarCarreraComponent },
+            { path: 'agregar', component: AgregarCarreraComponent },
+            { path: 'editar/:id', component: EditarCarreraComponent }
+        ]
+    },
 ]
--- src/app/modulos/administrativo/pages/carrera/pagesCarrera/agregar-carrera/agregar-carrera.component.ts
+++ src/app/modulos/administrativo/pages/carrera/pagesCarrera/agregar-carrera/agregar-carrera.component.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Carrera } from '../../../../modelos/carrera';
+import { UsuariosService } from '../../../../../administrativo/services-administrativo/usuarios.service';
 
 @Component({
   selector: 'app-agregar-carrera',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './agregar-carrera.component.html',
   styleUrl: './agregar-carrera.component.css'
 })
-export class AgregarCarreraComponent {
+export default class AgregarCarreraComponent implements OnInit {
+  formCarrera: FormGroup;
+  mensajeExito: string | null = null; // Para mostrar el mensaje
+  mensajeError: string | null = null; // Para errores
+  carreras: Carrera[] = [];
+  private readonly PATRON_NOMBRE = /^[A-Za-zÁÉÍÓÚáéíóúñÑ\s]+$/;
+  constructor(
+   
+    private fb: FormBuilder,
+    private usuariosService: UsuariosService
+  ) {
 
+    this.formCarrera = this.fb.group({
+      nombreCarrera: ['', [Validators.required, Validators.pattern(this.PATRON_NOMBRE)]],
+      idCarrera: ['', Validators.required],
+      fechaInscripcion: ['', Validators.required]
+    });
+  }
+  ngOnInit() {
+    this.usuariosService.getCarreras().subscribe({
+      next: (data) => this.carreras = data,
+      error: (err) => console.error('Error al cargar carreras:', err)
+    });
+  }
+  campoInvalido(campo: string): boolean {
+    const control = this.formCarrera.get(campo);
+    return control ? control.invalid && control.touched : false;
+  }
+  registrarCarrera() { // esto habilita o desabilita el botón para enviar el
+    if (!this.formCarrera.valid) {
+      console.log('Formulario inválido');
+      this.formCarrera.markAllAsTouched();
+      return;
+    }
+
+  }
 }
