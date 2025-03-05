71c7727b0bcbf1f912217cdda9c5494b45edb673
vfacu44 <vfacu44@gmail.com>
2025-03-05 14:25:57 +0000

    Configuración de variable global para conectar al backend

--- src/app/modulos/administrativo/services-administrativo/usuario.service.ts
+++ src/app/modulos/administrativo/services-administrativo/usuario.service.ts
@@ -1,20 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Usuario } from '../modelos/usuario';
+import {Alumno } from '../modelos/usuario';
+import { environment } from '../../../../environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 //esta parte nos permite conectarnosÑ con el back a través de http
 export class UsuarioService {
-  private urlBase ="https://8080-idx-backenditsu-1740021031173.cluster-kc2r6y3mtba5mswcmol45orivs.cloudworkstations.dev/itsuapi/usuarios";
+  private urlBase = environment.apiBaseUrl;
   constructor(private clienteHttp: HttpClient) { }
 
   //este método nos permite listar los alumnos a traves de una petición get
-  listarAlumnos(): Observable<Usuario[]>{
-    //devuelve un arreglo de tipo alumno pero envuelto en un observable
-    return this.clienteHttp.get<Usuario[]>(this.urlBase); 
+  listarAlumnos(): Observable<Alumno[]>{return this.clienteHttp.get<Alumno[]>(this.urlBase); 
   }
 }
 //NOTA: este servicio se utiliza desde el archivo "listar-alumnos.component.ts"
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -9,7 +9,7 @@ import { environment } from '../../../src/environments/environment';
   providedIn: 'root'
 })
 export class AuthService {
-  private urlBase = environment.apiBaseUrl || 'https://8080-idx-backenditsu-1740021031173.cluster-kc2r6y3mtba5mswcmol45orivs.cloudworkstations.dev/itsuapi';
+  private urlBase = environment.apiBaseUrl;
   private tokenKey = 'auth_token';// 
 
   constructor(private http: HttpClient) {}
