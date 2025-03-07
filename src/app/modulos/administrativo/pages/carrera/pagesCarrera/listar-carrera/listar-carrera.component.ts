5d90a7a1d041142196f59a94d38fd15383b21a4b
vfacu44work <vfacu44work@gmail.com>
2025-03-07 13:58:44 +0000

    feat: refactorizando listar carreras

--- src/app/modulos/administrativo/pages/carrera/pagesCarrera/listar-carrera/listar-carrera.component.ts
+++ src/app/modulos/administrativo/pages/carrera/pagesCarrera/listar-carrera/listar-carrera.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common'; // Importamos CommonModule
-import { CarreraService } from '../../../../services-administrativo/carrera.service';
+
 import { Carrera } from '../../../../modelos/carrera';
 import { RouterModule } from '@angular/router';
 
@@ -11,29 +11,12 @@ import { RouterModule } from '@angular/router';
   templateUrl: './listar-carrera.component.html',
   styleUrls: ['./listar-carrera.component.css']
 })
-export default class ListarCarreraComponent implements OnInit {
+export default class ListarCarreraComponent  {
   carreras: Carrera[] = [];
   loading: boolean = true;
   error: string | null = null;
 
-  constructor(private carreraService: CarreraService) {}
+ 
 
-  ngOnInit(): void {
-    this.cargarCarreras();
-  }
 
-  cargarCarreras(): void {
-    this.loading = true;
-    this.carreraService.listarCarreras().subscribe({
-      next: (data: Carrera[]) => {
-        this.carreras = data;
-        this.loading = false;
-      },
-      error: (err) => {
-        this.error = 'Error al cargar las carreras. Intenta de nuevo más tarde.';
-        console.error(err);
-        this.loading = false;
-      }
-    });
-  }
 }
