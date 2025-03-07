94df4246f0a6b6af1bd5edde0ed8a0b3aa3f0f49
vfacu44 <vfacu44@gmail.com>
2025-03-07 03:11:28 +0000

    feat: archivo de rutas de carreras correjido

--- src/app/modulos/administrativo/pages/carrera/carrera.routes.ts
+++ src/app/modulos/administrativo/pages/carrera/carrera.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import CarreraLayoutComponent  from './carrera-layout/carrera-layout.component';
 import AgregarCarreraComponent from './pagesCarrera/agregar-carrera/agregar-carrera.component';
-import { ListarCarreraComponent } from './pagesCarrera/listar-carrera/listar-carrera.component';
-import { EditarCarreraComponent } from './pagesCarrera/editar-carrera/editar-carrera.component';
+import ListarCarreraComponent  from './pagesCarrera/listar-carrera/listar-carrera.component';
+import {EditarCarreraComponent}  from './pagesCarrera/editar-carrera/editar-carrera.component';
 export const CARRERA_ROUTES: Routes = [
     {//al cargar la ruta base de carrera carga el layout de carrera y en diferido sus rutas hijas
         path: '', component: CarreraLayoutComponent,
